29a8bb143c006d65c7a4f6b88e4113b2ebbd4a01
moose <mooooseboi@gmail.com>
2024-03-11 11:01:01 +0200

    screenshots dont save to media

--- screenshot.sh
+++ screenshot.sh
@@ -3,13 +3,13 @@
 display="$(loginctl show-session self -p Type | sed 's'/Type=//)"
 
 [ $display == "x11" ] &&
-case $1 in
-  0) maim -s | xclip -selection clipboard -t image/png && notify-send -u low "Saving screenshot to clipboard";;
-  1) filename="$HOME/Media/screenshots/$(date).png" && maim -s $filename && notify-send -u low "Saving screenshot" "at $filename";;
-esac
+  case $1 in
+    0) maim | xclip -selection clipboard -t image/png && notify-send -u low "Saving screenshot to clipboard";;
+    1) maim -s | xclip -selection clipboard -t image/png && notify-send -u low "Saving screenshot to clipboard";;
+  esac
 
 [ $display == "wayland" ] &&
-case $1 in
-  1) grim -g "$(slurp)" | wl-copy && notify-send -u low "Saving screenshot to clipboard";;
-  2) grim -g "$(slurp)" -o "$HOME/Media/screenshots/$(date).png" && notify-send -u low "Saving screenshot" "at $filename";;
-esac
+  case $1 in
+    0) grim | wl-copy && notify-send -u low "Saving screenshot to clipboard";;
+    1) grim -g "$(slurp)" | wl-copy && notify-send -u low "Saving screenshot to clipboard";;
+  esac
