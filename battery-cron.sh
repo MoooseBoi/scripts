5cb7a846440d8063d7f7db85de15071fe77d6b20
moose <mooooseboi@gmail.com>
2024-05-06 15:38:51 +0300

    changed battery filename

--- battery-cron.sh
+++ battery-cron.sh
@@ -3,8 +3,8 @@
 export DISPLAY=:1
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-STATUS=`cat /sys/class/power_supply/BAT1/status`
-CAPACITY=`cat /sys/class/power_supply/BAT1/capacity`
+STATUS=`cat /sys/class/power_supply/BAT0/status`
+CAPACITY=`cat /sys/class/power_supply/BAT0/capacity`
 CRITICAL=10
 
 [ $CAPACITY -le $CRITICAL ] && [ "$STATUS" = "Discharging" ] && notify-send -u critical "Battery low" "Charge immediatly bozo >:T"
